7c16d22166962355b14c7423f14f1d52d3b6cfda
Shravan18v <ssravankumar89@gmail.com>
2025-06-10 22:42:56 +0530

    Create TaskModel.cs
    Model File For Task

---
+++ Model/TaskModel.cs
@@ -0,0 +1,7 @@
+public class TaskModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool Completed { get; set; }
+    public DateTime DueDate { get; set; }
+}
